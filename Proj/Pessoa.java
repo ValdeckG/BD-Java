7401f7b1fb8761ae2cea8495a2c88d5dcba7729a
ValdeckG <104246341+ValdeckG@users.noreply.github.com>
2024-03-12 20:17:45 -0300

    Pessoa.java

--- Proj/Pessoa.java
+++ Proj/Pessoa.java
@@ -15,7 +15,7 @@ public class Pessoa {
    
 
     Pessoa() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
 
    
